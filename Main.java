877c9ba9f8a701a770df41206866d2e8fc0b766a
bwmccauley <brandonmccauley@Brandons-MacBook-Pro.local>
2025-08-11 18:55:54 -0400

    added display

--- Main.java
+++ Main.java
@@ -3,17 +3,20 @@ import java.util.Scanner;
 public class Main {
 
         public static void main(String[] args) {
-//JAVA BANKING PROGRAM
+
 
 Scanner scanner = new Scanner(System.in);
 double balance; boolean isRunning = true;
 int choice;
 
-
-// DECLARE VARIABLES
-
-// DISPLAY MENU
-
+System.out.println("*********");
+System.out.println("BANKING PROGRAM");
+System.out.println("*********");
+System.out.println("1. Show Balance");
+System.out.println("2. Deposit");
+System.out.println("3. Withdraw");
+System.out.println("4. Exit");
+System.out.println("*********");
 // GEET AND PROCESS USERS CHOICE
 
 // showBalance()
