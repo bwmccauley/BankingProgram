b0fb218c5649df9130854fdbe7bb1d4e71b6727e
bwmccauley <brandonmccauley@Brandons-MacBook-Pro.local>
2025-08-11 18:47:50 -0400

    added variables

--- Main.java
+++ Main.java
@@ -1,3 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
 
+        public static void main(String[] args) {
+//JAVA BANKING PROGRAM
+
+Scanner scanner = new Scanner(System.in);
+double balance; boolean isRunning = true;
+int choice;
+
+
+// DECLARE VARIABLES
+
+// DISPLAY MENU
+
+// GEET AND PROCESS USERS CHOICE
+
+// showBalance()
+
+// deposit()
+
+
+// withdrawl()
+
+
+// EXIT MESSAGE
+
+scanner.close();
+        }
 }
