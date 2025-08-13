3fc0abfc5881cc310ce788d067130436be555bb6
bwmccauley <brandonmccauley@Brandons-MacBook-Pro.local>
2025-08-13 19:05:23 -0400

    added process

--- Main.java
+++ Main.java
@@ -17,7 +17,19 @@ System.out.println("2. Deposit");
 System.out.println("3. Withdraw");
 System.out.println("4. Exit");
 System.out.println("*********");
-// GEET AND PROCESS USERS CHOICE
+
+System.out.println("Enter your choice (1-4): ");
+choice = scanner.nextInt();
+
+        switch(choice){
+                case 1 -> System.out.println("SHOW BALANCE");
+                case 2 -> System.out.println("DEPOSIT");
+                case 3 -> System.out.println("WITHDRAW");
+                case 4 -> isRunning = false;
+                default -> System.out.println("INVALID CHOICE");
+        }
+
+
 
 // showBalance()
 
